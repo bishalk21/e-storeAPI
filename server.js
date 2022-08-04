bbc3b7cb00287ba0404ca67fc8b228597f890f30
Your Name <karkibishal00@gmail.com>
2022-08-04 15:48:40 +1000

    created backend api along with the packages

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 import express from "express"
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // 
 
 import cors from "cors";
 app.use(cors());
 
-const PORT = 8000;
+const PORT = process.env.PORT || 5000;
 
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send("Something broke!");
-})
-
-app.use("/", (req, res) => {
+app.use("/", (req, res) => { // app.use("/" for listening to all requests)
     res.send("Hello World");
 })
 
-app.listen(PORT, (err) => {
+app.use((err, req, res, next) => {
+    console.log(err);
+    // res.status(500).send("Something broke!");
+    const status = err.statusCode || 404;
+    res.status(status).json({
+        status: "error",
+        message: err.message
+    })
+})
+
+app.listen(PORT, (err) => { // app.listen(PORT,) for listening to specific port
     err && console.log(err);
     console.log(`Server is listening on port http://localhost:${PORT}`);
 })
